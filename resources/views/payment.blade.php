034eafedd0e8ae33468c1339230b98fa01851c2c
rifkyr990 <rifkir802@gmail.com>
2022-12-31 15:12:16 +0700

    add scan qr code

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -24,18 +24,50 @@
                 <div class="credit rounded mt-4 d-flex justify-content-between align-items-center">
                     <div class="d-flex flex-row align-items-center"> <img src="{{ asset('assets/image/dana.png') }}"
                             class="rounded bg-transparent p-2 ml-5" width="110">
-                        <div class="d-flex flex-column ml-3 mx-2"> <span class="business fw-bold">Rifky Ramadhan</span> <span
-                                class="plan">0895 3944 77132</span> </div>
+                        <div class="d-flex flex-column ml-3 mx-2"> <span class="business fw-bold">Rifky Ramadhan</span>
+                            <span class="plan">0895 3944 77132</span> </div>
                     </div>
-                    <div> <input type="text" class="form-control cvv" placeholder="CVC" hidden> </div>
+                    <div> <input type="button" class="form-control cvv" value="scan QR" data-bs-toggle="modal"
+                        data-bs-target="#staticBackdrop"> </div>
+                    <!-- Modal -->
+                    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+                        tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-body">
+                                <img class="d-block mx-auto" src="{{ asset('assets/image/dana-qr.jpg') }}" alt="" width="400px">
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary"
+                                        data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="credit rounded mt-2 d-flex justify-content-between align-items-center">
                     <div class="d-flex flex-row align-items-center"> <img src="{{ asset('assets/image/shopee.svg') }}"
                             class="rounded" width="110">
-                        <div class="d-flex flex-column ml-3 mx-2"> <span class="business fw-bold">Rifky Ramadhan</span> <span
-                                class="plan">0895 3944 77132</span> </div>
+                        <div class="d-flex flex-column ml-3 mx-2"> <span class="business fw-bold">Rifky Ramadhan</span>
+                            <span class="plan">0895 3944 77132</span> </div>
                     </div>
-                    <div> <input type="text" class="form-control cvv" placeholder="CVC" hidden> </div>
+                    <div> <input type="button" class="form-control cvv" value="scan QR" data-bs-toggle="modal"
+                        data-bs-target="#staticBackdrop"> </div>
+                    <!-- Modal -->
+                    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+                        tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-body">
+                                <img class="d-block mx-auto" src="{{ asset('assets/image/dana-qr.jpg') }}" alt="" width="400px">
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary"
+                                        data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <span class="detail fs-5 mt-3">Atau bisa dengan :</span>
                 <div class="credit rounded mt-3 d-flex justify-content-between align-items-center">
@@ -48,13 +80,13 @@
                         </div>
                     </div>
                 </div>
-                
-                <div class="mt-5"> 
+
+                <div class="mt-5">
                     <button class="btn btn-primary btn-block payment-button"><a href="https://wa.me/0895394477132"
                             class="text-light text-decoration-none">Konfirmasi Pembayaran <i
                                 class="fa fa-long-arrow-right"></i></a></button>
                     <button class="btn btn-danger btn-block payment-button mx-2"><a href="{{ url('home') }}"
-                            class="text-light text-decoration-none">Selesa <i
+                            class="text-light text-decoration-none">Selesai <i
                                 class="fa fa-long-arrow-right"></i></a></button></div>
             </div>
         </div>
